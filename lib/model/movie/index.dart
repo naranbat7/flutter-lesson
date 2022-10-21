d653a0715823894e6d2bf6d213d3ddf2755519d1
naranbat mainbayar <mnaranbat7@gmail.com>
2022-10-21 23:21:19 +0800

    feat: Movie Model serialization done

--- lib/model/movie/index.dart
+++ lib/model/movie/index.dart
@@ -1,1 +1,23 @@
-class MovieModel {}
+import 'package:json_annotation/json_annotation.dart';
+
+part 'index.g.dart';
+
+@JsonSerializable()
+class MovieModel {
+  int id;
+  String imgUrl;
+  int? publishedYear, durationMin;
+  String? type, description;
+
+  MovieModel({
+    required this.id,
+    required this.imgUrl,
+    this.description,
+    this.durationMin,
+    this.publishedYear,
+    this.type,
+  });
+
+  factory MovieModel.fromJson(Map<String, dynamic> json) => _$MovieModelFromJson(json);
+  Map<String, dynamic> toJson() => _$MovieModelToJson(this);
+}
---
+++ lib/model/movie/index.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'index.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+MovieModel _$MovieModelFromJson(Map<String, dynamic> json) => MovieModel(
+      id: json['id'] as int,
+      imgUrl: json['imgUrl'] as String,
+      description: json['description'] as String?,
+      durationMin: json['durationMin'] as int?,
+      publishedYear: json['publishedYear'] as int?,
+      type: json['type'] as String?,
+    );
+
+Map<String, dynamic> _$MovieModelToJson(MovieModel instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'imgUrl': instance.imgUrl,
+      'publishedYear': instance.publishedYear,
+      'durationMin': instance.durationMin,
+      'type': instance.type,
+      'description': instance.description,
+    };
